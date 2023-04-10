9916a22b4a0da968584ebd509ae93479cc856fd3
Mvikki@5786 <105000484+Vikki5786@users.noreply.github.com>
2023-04-10 19:54:01 +0530

    Add files via upload

---
+++ object_Casting/Casting.java
@@ -0,0 +1,12 @@
+
+public class Casting {
+	public static void main(String[] args) {
+		double x = 2.4;
+		int y = (int) x;
+		
+		double z = y;
+		
+		System.out.println(y);
+		System.out.println(x);
+	}
+}
